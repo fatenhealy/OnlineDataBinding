728cbfbc57f5b3bcc5697738f3ef58f3835af8c5
faten healy <faten.healy@telerik.com>
2014-12-02 15:28:28 +1100

    initial files

---
+++ ProductsWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsWebAPI.Models;
+
+namespace ProductsWebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id=1, ProductName="WebAPIProd1" },
+            new Product { Id=1, ProductName="WebAPIProd2" },
+            new Product { Id=1, ProductName="WebAPIProd3" }
+        };
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return products;
+        }
+    }
+}
